eb286c18e2265fb49e42ed610690cc51f99b8cfb
Geraldi2000 <62912661+Geraldi2000@users.noreply.github.com>
2024-08-31 23:26:59 -0300

    Forms
    adicionando a tela de formulario funcional

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
-from flask import Flask, render_template
-from random import randint
-ale = randint(0, 4)
-for p in range(5):
-    p = ale
+from flask import Flask, render_template, request, redirect
+#Cria uma classe
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+lista = []
+
 app = Flask(__name__)
+
 imagens = ['static/images.jpeg', 'static/Pericles.jpeg', 'static/wisky-1.png', 'static/wisky-2.jpg', 'static/wisky-3.jpg']
+
 @app.route('/')
 def hello_world():  # put application's code here
     list = [
-        {'Nome': 'Geraldi', 'imagem': imagens[p]},
-        {'Nome': 'Kaue2', 'imagem': imagens[p]},
-        {'Nome': 'Kaue3', 'imagem': imagens[p]},
-        {'Nome': 'Kaue4', 'imagem': imagens[p]},
-        {'Nome': 'Kaue5', 'imagem': imagens[p]}
+        {'Nome': 'Royal Salute', "Preco": "42000.00 R$", 'imagem': "static/images.jpg"},
+        {'Nome': 'Kaue2', "Preco": "42.00 R$", 'imagem': "static/images.jpg"},
+        {'Nome': 'Kaue3', "Preco": "42.00 R$", 'imagem': "static/images.jpg"},
+        {'Nome': 'Kaue4', "Preco": "42.00 R$", 'imagem': "static/images.jpg"},
+        {'Nome': 'Kaue5', "Preco": "42.00 R$", 'imagem': "static/images.jpg"}
     ]
-    return render_template('index.html', nomeLoja='Puro malte', cat=list)
-app.run()
+    return render_template('index.html', nomeLoja='Puro malte', cat=lista)
+    
+#Criando uma nova rota
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogos')
+
+@app.route("/form")
+def form():
+    return render_template("index.html")
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+app.run(debug=True)
+
